b48c8e4d5801a7b47b6320cdc41e7474599e30bc
Hassan Sheikh <hassansheikh9p9@gmail.com>
2024-07-08 14:47:19 +0500

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css';
-import Card from './Comonents/UI/Card/Card';
-import Input from './Comonents/Input/Input';
-import WeatherInfo from './Comonents/WeatherInfo/WeatherInfo';
-import { useCallback, useContext, useEffect } from 'react';
-import weatherContext from './context/weather';
-import useFetch from './hooks/useFetch';
+import "./App.css";
+import Card from "./Components/UI/Card/Card";
+import Input from "./Components/Input/Input";
+import WeatherInfo from "./Components/WeatherInfo/WeatherInfo";
+import { useCallback, useContext, useEffect } from "react";
+import weatherContext from "./context/weather";
+import useFetch from "./hooks/useFetch";
 
 export default function App() {
-
   const { error, isLoading, getWeather } = useFetch();
 
-  const dataFunc = useCallback(data => {
+  const dataFunc = useCallback((data) => {
     weatherCxt.setWeather({
       city: data.location.name,
       country: data.location.country,
@@ -19,7 +18,7 @@ export default function App() {
       temp: data.current.temp_c,
       icon: data.current.condition.icon,
       windSpeed: data.current.wind_kph,
-    })
+    });
   }, []);
 
   let content;
@@ -27,26 +26,31 @@ export default function App() {
   const weatherCxt = useContext(weatherContext);
 
   function getLocation() {
-    navigator.geolocation.getCurrentPosition(position => {
-      const lat = position.coords.latitude;
-      const lon = position.coords.longitude;
-      getWeather('current', dataFunc, `&q=${lat},${lon}&aqi=no`);
-    }, () => {
-      content = <h2>Allow the location or enter a Place to Continue</h2>
-    })
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        getWeather("current", dataFunc, `&q=${lat},${lon}&aqi=no`);
+      },
+      () => {
+        content = <h2>Allow the location or enter a Place to Continue</h2>;
+      }
+    );
   }
 
   useEffect(() => {
     getLocation();
   }, []);
 
-  return <>
-    <Card className='container'>
-      <Input dataFunc={dataFunc} onSubmit={getWeather}></Input>
-      {isLoading && !error && <h2>Getting Current Weather...</h2>}
-      {error && !isLoading && <h2>Error: {error.message}</h2>}
-      {content}
-      {!error && !isLoading && <WeatherInfo />}
-    </Card>  </>
-
+  return (
+    <>
+      <Card className="container">
+        <Input dataFunc={dataFunc} onSubmit={getWeather}></Input>
+        {isLoading && !error && <h2>Getting Current Weather...</h2>}
+        {error && !isLoading && <h2>Error: {error.message}</h2>}
+        {content}
+        {!error && !isLoading && <WeatherInfo />}
+      </Card>{" "}
+    </>
+  );
 }
