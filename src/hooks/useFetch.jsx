a99d4d5eb9f23710091514fc84a2d150dc254ef7
Hassan Sheikh <hassansheikh9p9@gmail.com>
2024-07-08 14:43:37 +0500

    changed API key and fixed a folder name

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
 
 export default function useFetch() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    async function getWeather(check, dataFunc, url) {
-        try {
-            setIsLoading(true);
-            setError(null);
-            const response = await fetch(
-                `http://api.weatherapi.com/v1/${check}.json?key=d0efeca2d39e4df7b2f60107240606${url}`);
-            if (response.status === 400) {
-                throw new Error('City/Country not Found ! Try another one');
-            }
-            if (!response.ok) {
-                throw new Error(`Some Error occured : ${response.statusText}`)
-            }
-            const data = await response.json();
-            dataFunc(data);
-        }
-        catch (err) {
-            console.log(err);
-            setError(err)
-        }
-        setIsLoading(false);
+  async function getWeather(check, dataFunc, url) {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/${check}.json?key=e72b8a43abe84491a4394027240807${url}`
+      );
+      if (response.status === 400) {
+        throw new Error("City/Country not Found ! Try another one");
+      }
+      if (!response.ok) {
+        throw new Error(`Some Error occured : ${response.statusText}`);
+      }
+      const data = await response.json();
+      dataFunc(data);
+    } catch (err) {
+      console.log(err);
+      setError(err);
     }
+    setIsLoading(false);
+  }
 
-    return {
-        error,
-        isLoading,
-        getWeather,
-    }
+  return {
+    error,
+    isLoading,
+    getWeather,
+  };
 }
