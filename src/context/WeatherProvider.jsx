1810fe9c5623ad5114bfd9aa0567db515ff21e62
Hassan Sheikh <hassansheikh9p9@gmail.com>
2024-07-08 17:50:16 +0500

    display message if API key is expired

--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -2,69 +2,84 @@ import React, { useReducer } from "react";
 import weatherContext from "./weather";
 
 let initWeather = {
-    city: '',
-    country: '',
-    time: '',
-    weather: '',
-    temp: '',
-    icon: '',
-    windSpeed: '',
-    forecast: [],
-}
+  city: "",
+  country: "",
+  time: "",
+  weather: "",
+  temp: "",
+  icon: "",
+  windSpeed: "",
+  forecast: [],
+  expired: false,
+};
 
 function weatherReducer(prevState, action) {
+  if (action.type === "OK") {
+    return {
+      ...prevState,
+      ...action.value,
+    };
+  }
+  if (action.type === "INPUT") {
+    return {
+      ...prevState,
+      city: action.value,
+    };
+  }
+  if (action.type === "FORECAST") {
+    return {
+      ...prevState,
+      forecast: action.value,
+    };
+  }
+  if (action.type === "EXPIRED") {
+    return {
+      ...prevState,
+      expired: action.value,
+    };
+  }
 
-    if (action.type === 'OK') {
-        return {
-            ...prevState,
-            ...action.value
-        }
-    } if (action.type === 'INPUT') {
-        return {
-            ...prevState,
-            city: action.value
-        }
-    } if (action.type === 'FORECAST') {
-        return {
-            ...prevState,
-            forecast: action.value
-        }
-    }
-
-    return initWeather;
+  return initWeather;
 }
 
 export default function WeatherProvider(props) {
+  const [state, dispatchWeather] = useReducer(weatherReducer, initWeather);
 
-    const [state, dispatchWeather] = useReducer(weatherReducer, initWeather);
+  function setWeather(weatherObj) {
+    dispatchWeather({ type: "OK", value: weatherObj });
+  }
 
-    function setWeather(weatherObj) {
-        dispatchWeather({ type: 'OK', value: weatherObj });
-    }
+  function setCity(city) {
+    dispatchWeather({ type: "INPUT", value: city });
+  }
 
-    function setCity(city) {
-        dispatchWeather({ type: 'INPUT', value: city });
-    }
+  function setForecast(forecastArr) {
+    dispatchWeather({ type: "FORECAST", value: forecastArr });
+  }
+  function setExpired(status) {
+    dispatchWeather({ type: "EXPIRED", value: status });
+  }
 
-    function setForecast(forecastArr) {
-        dispatchWeather({ type: 'FORECAST', value: forecastArr })
-    }
+  const weatherCxt = {
+    city: state.city,
+    data: state.data,
+    country: state.country,
+    forecast: state.forecast,
+    time: state.time,
+    weather: state.weather,
+    temp: state.temp,
+    icon: state.icon,
+    windSpeed: state.windSpeed,
+    setWeather,
+    setCity,
+    setForecast,
+    expired: state.expired,
+    setExpired,
+  };
 
-
-    const weatherCxt = {
-        city: state.city,
-        data: state.data,
-        country: state.country,
-        forecast: state.forecast,
-        time: state.time,
-        weather: state.weather,
-        temp: state.temp,
-        icon: state.icon,
-        windSpeed: state.windSpeed,
-        setWeather,
-        setCity,
-        setForecast,
-    }
-
-    return <weatherContext.Provider value={weatherCxt}>{props.children}</weatherContext.Provider>
+  return (
+    <weatherContext.Provider value={weatherCxt}>
+      {props.children}
+    </weatherContext.Provider>
+  );
 }
--- src/context/weather.jsx
+++ src/context/weather.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 const weatherContext = React.createContext({
-    city: '',
-    country: '',
-    forecast: [],
-    time: '',
-    weather: '',
-    temp: '',
-    icon: '',
-    windSpeed: '',
-    setWeather: (weatherObj) => { },
-    setCity: (city) => { },
-    setForecast: (forecastArr) => { },
-})
+  city: "",
+  country: "",
+  forecast: [],
+  time: "",
+  weather: "",
+  temp: "",
+  icon: "",
+  windSpeed: "",
+  setWeather: (weatherObj) => {},
+  setCity: (city) => {},
+  setForecast: (forecastArr) => {},
+  expired: false,
+  setExpired: (status) => {},
+});
 export default weatherContext;
